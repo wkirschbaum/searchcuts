26e108d297386045161273d8fc03406e785d5ff2
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2015-08-14 22:07:49 +0200

    initial working version

---
+++ content.js
@@ -0,0 +1,11 @@
+
+$('h3.r').each(function( index) {
+  var shortcut_number = (index + 1) % 10;
+  $(this).prepend( "press " + shortcut_number );
+  var link = $(this).children('a').attr('href');
+  $(document).keypress(function( event ) {
+    if ( event.which == 48 + shortcut_number ) {
+      location.href = link;
+    }
+  });
+});
