d8989f68f3e7fff574de371426c1e13bf2bf61d8
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2015-08-14 22:09:40 +0200

    space hint

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 
 $('h3.r').each(function( index) {
   var shortcut_number = (index + 1) % 10;
-  $(this).prepend( "press " + shortcut_number );
+  $(this).prepend( "press " + shortcut_number +": " );
   var link = $(this).children('a').attr('href');
   $(document).keypress(function( event ) {
     if ( event.which == 48 + shortcut_number ) {
