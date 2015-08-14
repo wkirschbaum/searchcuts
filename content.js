4eb8b385f6d0fc6e4a2842d11652b1318655756b
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2015-08-14 22:57:51 +0200

    move shortcut to the left of the title

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 $('h3.r').each(function( index) {
   var shortcut_number = (index + 1) % 10;
-  $(this).prepend( "press " + shortcut_number +": " );
+  $(this).before( "<div style='position: absolute; left: -13px; top: 3px;'>" + shortcut_number +": <div>" );
   var link = $(this).children('a').attr('href');
   $(document).keypress(function( event ) {
     if ( event.which == 48 + shortcut_number ) {
